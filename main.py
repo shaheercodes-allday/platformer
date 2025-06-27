2cd6492d142ee4b9a18f03020e383465ccf38db4
Shaheer Shaikh <shaheercodes.allday@gmail.com>
2025-06-27 16:34:03 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from sys import exit
+import pygame as pg
+
+pg.init()
+
+FPS: int = 60
+CLOCK = pg.time.Clock()
+WIDTH, HEIGHT = 1000, 780
+WIN = pg.display.set_mode((WIDTH, HEIGHT))
+
+pg.display.set_caption('PLATFORMER')
+
+def main() -> None:
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                exit()
+
+        pg.display.update()
+        CLOCK.tick(FPS)
+
+if __name__ == '__main__':
+    main()
